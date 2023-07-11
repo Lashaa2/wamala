c7425be7471bc0c760f6567a33778a6de92e9974
Lashaa2 <135804697+Lashaa2@users.noreply.github.com>
2023-07-11 22:43:35 +0400

    lesson2.py

--- day 2/lesson2.py
+++ day 2/lesson2.py
@@ -3,8 +3,10 @@ name = "Lasha"
 surname = "Tsamalaidze"
 Age = "13"
 height = "158"
-print("height" + Age)
+print("height" , Age)
 print (name + surname)
 print( 13 + 158)
 print(13 * 158)
 print(158 ** 13)
+  print("goa sauketeso akademiaa")
+print("madloba gabriel ")
