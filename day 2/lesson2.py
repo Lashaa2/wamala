78ef9c9f793aa227ad460b5e13c993f139d97cd7
Lashaa2 <135804697+Lashaa2@users.noreply.github.com>
2023-07-11 22:08:11 +0400

    lesson2.py

--- day 2/lesson2.py
+++ day 2/lesson2.py
@@ -1,1 +1,10 @@
 print("lasha wamalaidze learning in goa")
+name = "Lasha"
+surname = "Tsamalaidze"
+Age = "13"
+height = "158"
+print("height" + "Age")
+print (name + surname)
+print( 13 + 158)
+print(13 * 158)
+print(158 ** 13)
